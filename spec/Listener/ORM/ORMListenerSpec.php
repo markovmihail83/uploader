a2c2bab789e65b707895bfeb5b574654ed941aed
Elbek Azimov <atom.azimov@gmail.com>
2016-02-20 11:45:03 +0500

    Add spec for ORM listener

---
+++ spec/Listener/ORM/ORMListenerSpec.php
@@ -0,0 +1,108 @@
+<?php
+
+
+namespace spec\Atom\Uploader\Listener\ORM;
+
+
+use Atom\Uploader\Handler\ListenerHandler;
+use Atom\Uploader\Listener\ORM\ORMListener;
+use Atom\Uploader\Model\Embeddable\FileReference;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Events;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * @mixin ORMListener
+ */
+class ORMListenerSpec extends ObjectBehavior
+{
+    private $events;
+
+    function let(
+        ListenerHandler $handler,
+        PreUpdateEventArgs $event,
+        FileReference $fileReference,
+        EntityManagerInterface $em,
+        ClassMetadata $metadata
+    ) {
+        $this->events = [
+            Events::prePersist,
+            Events::postPersist,
+            Events::preUpdate,
+            Events::postUpdate,
+            Events::postLoad,
+            Events::postRemove,
+            Events::postFlush,
+        ];
+
+        $fileReferenceEntities = [
+            get_class($fileReference->getWrappedObject()) => $fileReference
+        ];
+
+        $this->beConstructedWith($handler, $fileReferenceEntities, $this->events);
+
+        $event->getEntity()->willReturn($fileReference);
+        $event->getEntityManager()->willReturn($em);
+        $em->getClassMetadata(Argument::type('string'))->willReturn($metadata);
+        
+        $event->getEntityChangeSet()->willReturn([
+            'file',
+            'some-embeddable.field',
+            'uri',
+            'file_info',
+        ]);
+
+        $event->getOldValue('file')->willReturn('file');
+        $event->getOldValue('file_info')->willReturn('file_info');
+        $event->getOldValue('uri')->willReturn('uri');
+
+        $metadata->setFieldValue(Argument::any(), Argument::type('string'), Argument::any())->willReturn(null);
+    }
+
+    function it_should_get_the_events()
+    {
+        $this->getSubscribedEvents()->shouldEqual($this->events);
+    }
+
+    function it_should_do_nothing_when_entity_is_not_file_reference($handler, $event, $notFileReference)
+    {
+        $event->getEntity()->willReturn($notFileReference);
+        $handler->prePersist(Argument::any(), Argument::any())->shouldNotBeCalled();
+        $handler->postPersist(Argument::any())->shouldNotBeCalled();
+        $handler->preUpdate(Argument::any(), Argument::any(), Argument::any())->shouldNotBeCalled();
+        $handler->postUpdate(Argument::any())->shouldNotBeCalled();
+        $handler->postRemove(Argument::any())->shouldNotBeCalled();
+        $handler->postLoad(Argument::any())->shouldNotBeCalled();
+
+        $this->prePersist($event);
+        $this->postPersist($event);
+        $this->preUpdate($event);
+        $this->postUpdate($event);
+        $this->postLoad($event);
+        $this->postRemove($event);
+    }
+
+    function it_should_call_same_method_of_listener_handler(ListenerHandler $handler, $event, $fileReference)
+    {
+        $id = spl_object_hash($fileReference->getWrappedObject());
+        
+        $handler->prePersist($id, $fileReference)->shouldBeCalled();
+        $handler->postPersist($id)->shouldBeCalled();
+        $handler->preUpdate($id, $fileReference, Argument::type(FileReference::class))->shouldBeCalled();
+        $handler->postUpdate($id)->shouldBeCalled();
+        $handler->postRemove($fileReference)->shouldBeCalled();
+        $handler->postLoad($fileReference)->shouldBeCalled();
+        $handler->postFlush()->shouldBeCalled();
+
+        $this->prePersist($event);
+        $this->postPersist($event);
+        $this->preUpdate($event);
+        $this->postUpdate($event);
+        $this->postLoad($event);
+        $this->postRemove($event);
+        $this->postFlush();
+    }
+}
